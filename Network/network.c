c94059d01052df6abf055f1c11b831208e6df959
Pierre <pierre.vidal@epita.fr>
2016-10-19 18:37:43 +0200

    Network as struct

--- Network/network.c
+++ Network/network.c
@@ -1,23 +1,22 @@
 // network.c
 # include <stdio.h>
 
-int networkLayers;
-int networkSizes[3];
-int biases[2];
-int weights[2];
-
-void initNetword(int sizes[], int layers){
-    // Init number of layers
-	networkLayers = layers;
-    //Init sizes of layers
-    for(int i = 0; i < layers; ++i){
-        networkSizes[i] = sizes[i];
-    }
-}
-
+struct Network{
+    int numLayers;
+    int sizes[3];
+    float biases[2];
+    float weight[2];
+};
 
 int main(){
-	int sizes[] = {2, 3, 1};
-	initNetword(sizes, 3);
+    //Init Network
+    struct Network network;
+    // Init number of layers
+    network.numLayers = 3;
+    //Init sizes of the layers
+    int sizes[] = {2, 3, 1};
+    for(int i = 0; i < network.numLayers; ++i){
+        network.sizes[i] = sizes[i];
+    }
 	return 0;
 }
